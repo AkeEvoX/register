19955e63db34a5776f0c29a3511889e909bad89e
AkeEvoX <werawat.l@icloud.com>
2016-06-30 22:59:32 +0700

    update register
    post serialise to object

--- process/register.php
+++ process/register.php
@@ -1,49 +1,54 @@
-<?php
+<?php 
+require_once("../class/connecter.php");
 header('Content-Type: application/json');
 session_start();
-include ("../class/connecter.php");
+
 
 //get current profile
-$inputJSON = file_get_contents('php://input');
-$item= json_decode( stripslashes($inputJSON),true); //convert JSON into array
+$data = json_decode(json_encode($_POST));
+//$data = $data->selectSize;
 
 /*
-
-inputTitile=1&InputFirstName=tes&InputLastName=test&InputAge=12&InputPhone=31231
-&InputPhoneEmer=1231&InputAddress=sdf&InputEmail=s%40f
-&InputLocation=Location+A
+inputTitle=1
+&InputFirstName=test
+&InputLastName=test
+&InputAge=12
+&InputPhone=31231
+&InputPhoneEmer=1231
+&InputAddress=sdf
+&InputEmail=s%40f
+&InputLocation=A
 &InputDisease=wefwe
 &inputBlood=A
 &selectSize=M
 
 */
-$milliseconds = round(microtime(true) * 1000);
 
-	$title = $item["inputTitile"];
-	$fname = $item["InputFirstName"];
-	$lname = $item["lname"];
-	$age = $item["age"];
-	$phone = $item["InputPhone"];
-	$emerphone = $item["InputPhoneEmer"];
-	$address  = $item["InputAddress"];
-	$email =  $item["InputEmail"];
-	$location = $item["InputLocation"];
-	$disease = $item["InputDisease"];
-	$blood = $item["inputBlood"];
-	$size = $item["selectSize"];
-	$token = "token=".$milliseconds ;
+	$milliseconds = round(microtime(true) * 1000);
+
+	$title = $data->inputTitle;
+	$fname = $data->InputFirstName;
+	$lname = $data->InputLastName;
+	$age = $data->InputAge;
+	$phone = $data->InputPhone;
+	$emerphone = $data->InputPhoneEmer;
+	$address  = $data->InputAddress;
+	$email =  $data->InputEmail;
+	$location = $data->InputLocation;
+	$disease = $data->InputDisease;
+	$blood = $data->inputBlood;
+	$size = $data->selectSize;
+	$token = $milliseconds ;
 	
-	$result  =  $item;
+	$result  =  "Failed";
 	
 
 /* check duplicate */
 
-//print_r($item);
-
 /* check concurent forregister */
 
 	try{	
-	/*
+	
 		$call = $consqli->prepare("call registeruser(?,?,?,?,?,?,?,?,?,?,?,?,?) ;");
 		$call->bind_param('sssisssssssss',$title,$fname,$lname,$age,$phone,$emerphone,$address,$email,$location,$disease,$blood,$size,$token);
 		$call->execute();
@@ -51,15 +56,15 @@ $milliseconds = round(microtime(true) * 1000);
 		
 		$call->close();
 		$consqli->close();
-		*/
+		
 	}
 	catch(Exception $e)
 	{
 		$result  = "Service crash!! ";
-		//die("Insert Fails.");
+		die("Service Unvailable");
 	}
 
-echo json_encode(array("result"=> $result ,"code"=>"0","param"=>$fname));
+echo json_encode(array("result"=> $result ,"code"=>"0","param"=>$data));
 //print_r(json_encode($item)); 
 
 ?>
